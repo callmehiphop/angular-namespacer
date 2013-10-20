4f1fe61a79bb6c171dd82dbd1b9c48a4f4b6a0cd
callmehiphop <callmehiphop@gmail.com>
2013-10-20 03:25:48 -0400

    backed out flag commit

--- angular.namespacer.js
+++ angular.namespacer.js
@@ -10,24 +10,22 @@
   var _module = angular.module;
   var methods = 'constant decorator factory provider service value'.split(' ');
 
-  angular.module = function(name, requires, configFn, namespace) {
-    var moduleInstance = _module.call(angular, name, requires, configFn);
-    
-    if (namespace) {
-      angular.forEach(methods, function(method) {
-        var _method = moduleInstance[method];
-  
-        moduleInstance[method] = function(providerName) {
-          var args = slice.call(arguments);
-        
-          if (!/^(\$|[ng]+)/.test(providerName)) {
-            args[0] = name + '.' + providerName;
-          }
-  
-          return _method.apply(moduleInstance, args);
-        };
-      });
-    }
+  angular.module = function(name) {
+    var moduleInstance = _module.apply(angular, arguments);
+
+    angular.forEach(methods, function(method) {
+      var _method = moduleInstance[method];
+
+      moduleInstance[method] = function(providerName) {
+        var args = slice.call(arguments);
+      
+        if (!/^(\$|[ng]+)/.test(providerName)) {
+          args[0] = name + '.' + providerName;
+        }
+
+        return _method.apply(moduleInstance, args);
+      };
+    });
 
     return moduleInstance;
   };
